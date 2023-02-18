ecf016e7b2cf89609cc55de2ce677faf56c41a76
WOWmasterJulia <topazzz@ukr.net>
2023-02-19 00:17:22 +0200

    Update test.js

--- test.js
+++ test.js
@@ -895,3 +895,21 @@
 // Виклик includes() для випадкового масиву з випадковим value повертає правильний boolean
 // У функції includes використовується for, return, але не метод масиву includes
 
+function includes(array, value) {
+  
+  // Change code below this line
+for (let i = 0; i < array.length; i +=1) {
+  if array[i] = value {
+    return true;
+  }
+}
+  return false;
+  // Change code above this line
+}
+
+console.log(includes([1, 2, 3, 4, 5], 3));
+console.log(includes([1, 2, 3, 4, 5], 17));
+console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
+console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus"));
+console.log(includes(["apple", "plum", "pear", "orange"], "plum"));
+console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
