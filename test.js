3b494053e524b305ce88e51e68d21bd4d1882fd1
WOWmasterJulia <topazzz@ukr.net>
2023-02-19 12:57:24 +0200

    Update test.js

--- test.js
+++ test.js
@@ -899,7 +899,7 @@ function includes(array, value) {
   
   // Change code below this line
 for (let i = 0; i < array.length; i +=1) {
-  if array[i] = value {
+  if (array[i] === value) {
     return true;
   }
 }
