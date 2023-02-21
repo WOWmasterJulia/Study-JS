006a59faea4db93694a5cf4005acc61995339018
WOWmasterJulia <topazzz@ukr.net>
2023-02-21 23:45:43 +0200

    Update test.js

--- test.js
+++ test.js
@@ -940,3 +940,102 @@
 
 // console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
 // console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+
+// 17\41
+// Перебери масив об'єктів colors, використовуючи цикл for...of. Додай у масив hexColors значення властивостей hex, а в масив rgbColors - значення властивостей rgb з усіх об'єктів масиву colors.
+
+// Оголошена змінна colors
+// Значення змінної colors - це масив
+// Оголошена змінна hexColors
+// Значення змінної hexColors - це масив ["#f44336", "#2196f3", "#4caf50", "#ffeb3b"]
+// Оголошена змінна rgbColors
+// Значення змінної rgbColors - це масив ["244,67,54", "33,150,243", "76,175,80", "255,235,59"]
+// const colors = [
+//   { hex: "#f44336", rgb: "244,67,54" },
+//   { hex: "#2196f3", rgb: "33,150,243" },
+//   { hex: "#4caf50", rgb: "76,175,80" },
+//   { hex: "#ffeb3b", rgb: "255,235,59" },
+// ];
+
+// const hexColors = [];
+// const rgbColors = [];
+// // console.log(hexColors);
+// // console.log(rgbColors);
+
+// for (const color of colors) {
+//   hexColors.push(color.hex);
+//   rgbColors.push(color.rgb);
+// }
+// console.log(hexColors);
+// console.log(rgbColors);
+
+
+
+// Завдання 18/41
+// Напиши функцію getProductPrice(productName), яка приймає один параметр 
+// productName - назва продукту. Функція шукає об'єкт продукту з таким ім'ям 
+// (властивість name) в масиві products і повертає його ціну (властивість price). 
+// Якщо продукт з такою назвою не знайдений, функція повинна повертати null.
+
+// Оголошена функція getProductPrice(productName).
+// Виклик getProductPrice("Radar") повертає 1300.
+// Виклик getProductPrice("Grip") повертає 1200.
+// Виклик getProductPrice("Scanner") повертає 2700.
+// Виклик getProductPrice("Droid") повертає 400.
+// Виклик getProductPrice("Engine") повертає null.
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// function getProductPrice(productName) {
+//   let result = null;
+//   for (const i of products) {
+    
+//     if (i.name === productName) {
+//       result = i.price;
+//     }
+//   }
+//   return result;
+// }
+// console.log(getProductPrice("Radar"));
+// console.log(getProductPrice("Grip"));
+// console.log(getProductPrice("Scanner"));
+// console.log(getProductPrice("Droid"));
+// console.log(getProductPrice("Engine"));
+
+
+// 19\41
+// Напиши функцію getAllPropValues(propName), яка приймає один параметр propName - ім'я (ключ) властивості. Функція повинна повернути масив всіх значень властивості з таким ім'ям з кожного об'єкта в масиві products. Якщо в об'єктах відсутні властивості з таким ім'ям, функція повинна повернути порожній масив.
+
+// Оголошена функція getAllPropValues(propName)
+// Виклик getAllPropValues("name") повертає ["Radar", "Scanner", "Droid", "Grip"]
+// Виклик getAllPropValues("quantity") повертає [4, 3, 7, 9]
+// Виклик getAllPropValues("price") повертає [1300, 2700, 400, 1200]
+// Виклик getAllPropValues("category") повертає []
+
+// const products = [
+//   { name: "Radar", price: 1300, quantity: 4 },
+//   { name: "Scanner", price: 2700, quantity: 3 },
+//   { name: "Droid", price: 400, quantity: 7 },
+//   { name: "Grip", price: 1200, quantity: 9 },
+// ];
+
+// console.log(products);
+// function getAllPropValues(propName) {
+//   let result = [];
+//   for (let i = 0; i < products.length; i += 1) {
+//     if (products[i][propName]) {
+//       result.push(products[i][propName]);
+//     }
+//   }
+//   return result;
+// }
+
+// console.log(getAllPropValues("name"));
+// console.log(getAllPropValues("quantity"));
+// console.log(getAllPropValues("price"));
+// console.log(getAllPropValues("category"));
