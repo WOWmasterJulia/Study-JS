067380d0958cc662e87bc953ab10b45da17216c1
WOWmasterJulia <topazzz@ukr.net>
2023-02-21 14:26:01 +0200

    Update test.js

--- test.js
+++ test.js
@@ -895,21 +895,38 @@
 // Виклик includes() для випадкового масиву з випадковим value повертає правильний boolean
 // У функції includes використовується for, return, але не метод масиву includes
 
-function includes(array, value) {
+// function includes(array, value) {
   
-  // Change code below this line
-for (let i = 0; i < array.length; i +=1) {
-  if array[i] = value {
-    return true;
-  }
-}
-  return false;
-  // Change code above this line
+//   // Change code below this line
+// for (let i = 0; i < array.length; i +=1) {
+//   if array[i] = value {
+//     return true;
+//   }
+// }
+//   return false;
+//   // Change code above this line
+// }
+
+// console.log(includes([1, 2, 3, 4, 5], 3));
+// console.log(includes([1, 2, 3, 4, 5], 17));
+// console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
+// console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus"));
+// console.log(includes(["apple", "plum", "pear", "orange"], "plum"));
+// console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
+
+function countTotalSalary(salaries) {
+  let totalSalary = 0;
+
+// const keys = Object.keys(salariest);
+console.log(salaries);
+const values = Object.values(salaries);
+console.log(values);
+   for (const value of values) {
+  totalSalary += value 
+   }
+  return totalSalary;
+
 }
 
-console.log(includes([1, 2, 3, 4, 5], 3));
-console.log(includes([1, 2, 3, 4, 5], 17));
-console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Jupiter"));
-console.log(includes(["Earth", "Mars", "Venus", "Jupiter", "Saturn"], "Uranus"));
-console.log(includes(["apple", "plum", "pear", "orange"], "plum"));
-console.log(includes(["apple", "plum", "pear", "orange"], "kiwi"));
+console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
